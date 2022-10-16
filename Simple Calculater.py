546bb9cb75b1c48c315c545ba4a51eb3bd7d293b
EshanRiyas <115924559+EshanRiyas@users.noreply.github.com>
2022-10-16 14:00:07 +0400

    Add files via upload

---
+++ Simple Calculater.py
@@ -0,0 +1,13 @@
+x = int(input("Enter the first number= "))
+y = int(input("Enter the second number= "))
+sum = x + y
+difference = x - y
+product = x * y 
+quotient = x / y
+remainder = x % y
+print("Simple Calculator")
+print("Sum of x and y is " , sum)
+print("Difference of x and y is " , difference)
+print("Product of x and y is " , product)
+print("Quotient of x and y is " , quotient)
+print("Remainder of x and y is " , remainder)
